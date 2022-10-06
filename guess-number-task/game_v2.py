7c24261c9d872740835c313d7643547dff3e8d06
andycraft <andy@andycraft.ru>
2022-10-06 11:01:40 +0600

    Added binary search

--- guess-number-task/game_v2.py
+++ guess-number-task/game_v2.py
@@ -24,8 +24,43 @@ def random_predict(number: int = 1) -> int:
     return count
 
 
-def score_game(random_predict) -> int:
-    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+def binary_search(number: int = 1) -> int:
+    """Находим заданное число методом бинарного поиска
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+
+    # задаем верхнюю и нижнюю границы искомого числа
+    low_value, high_value = 1, 100
+
+    while True:
+        count += 1
+
+        # определяем среднее арифметическое значение
+        predict_number = (low_value + high_value) // 2
+
+        if number > predict_number:
+            # искомое число больше предсказанного, сдвигаем нижнюю границу
+            low_value = predict_number + 1
+
+        elif number < predict_number:
+            # искомое число меньше предсказанного, сдвигаем верхнюю границу
+            high_value = predict_number - 1
+
+        else:
+            # число найдено, выходим из цикла
+            break
+
+    return count
+
+
+def score_game(predict_function) -> int:
+    """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
 
     Args:
         random_predict ([type]): функция угадывания
@@ -34,17 +69,20 @@ def score_game(random_predict) -> int:
         int: среднее количество попыток
     """
     count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
-        count_ls.append(random_predict(number))
+        count_ls.append(predict_function(number))
 
     score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    print(f"Функция {predict_function.__name__} угадывает число",
+          f"в среднем за: {score} попыток")
+
     return score
 
 
 if __name__ == "__main__":
     # RUN
     score_game(random_predict)
+    score_game(binary_search)
